110b4b76ef35d792535d41f3b0ac224a92aa2d47
justinmutinta <justinmutinta@hotmail.com>
2024-09-28 20:04:34 -0500

    playing with jinja values

--- paperwork/projects/views.py
+++ paperwork/projects/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def projects(request):
-    return render(request, 'projects.html')
+    msg = 'Hello, you are on the projects view'
+    return render(request, 'projects.html', {'message': msg})
 
 def project(request, pk):
     return render(request, 'single-project.html')
