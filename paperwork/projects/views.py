ae23cc9858720cd3e48a5870a58c16c7f989e619
justinmutinta <justinmutinta@hotmail.com>
2024-10-03 21:42:02 -0500

    playing with lists

--- paperwork/projects/views.py
+++ paperwork/projects/views.py
@@ -4,11 +4,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+projectsList = [
+    {
+        'id' : '1',
+        'title' : "Ecommerce Website",
+        'description': 'Fully Functional ecommerce website'
+    },
+    {
+        'id' : '2',
+        'title' : "Portfolio Website",
+        'description': 'This was a project where I built out my portfolio'
+    },
+    {
+        'id' : '3',
+        'title' : "Social Network",
+        'description': 'Awesome open source project I am still working on'
+    },
+]
+
+
 def projects(request):
     page = 'projects'
     number = 11
-    context = {'page': page, 'number': number}
+    context = {'page': page, 'number': number, 'projects': projectsList}
     return render(request, 'projects.html', context)
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'single-project.html', {'project':projectObj})
