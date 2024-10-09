30612d9eff59be57804b84fcabc640ef76fde95f
justinmutinta <justinmutinta@hotmail.com>
2024-10-08 22:21:16 -0500

    Able to delete

--- paperwork/messwithpaperwork/views.py
+++ paperwork/messwithpaperwork/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
@@ -16,6 +17,14 @@ def home(request):
     testSets = TestSet.objects.all()
     return render(request, 'home.html', {'testSets': testSets})
 
+
 def testSet(request, pk):
     testSet = TestSet.objects.get(id=pk)
     return render(request, 'testSet.html', {'testSet': testSet})
+
+
+def delete_testset(request, pk):
+    delete_it = TestSet.objects.get(id=pk)
+    delete_it.delete()
+    messages.success(request, "You have deleted the test set successfully")
+    return redirect('home')
